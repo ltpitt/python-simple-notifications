f189ba85f0bfc9591276c460188b6f60e5a6014a
Davide Nastri <d.nastri@gmail.com>
2016-05-24 16:27:22 +0200

    feat: corrected display_help() output

--- notification.py
+++ notification.py
@@ -99,8 +99,8 @@ def display_help():
     '''
     This functions displays the command help
     '''
-    print 'Email Example:     --email "Email Subject", "Email Message", "Email recipients"'
-    print 'Pusbullet Example: --pushbullet "Title", "Message"'
+    print 'Email Example:     --email "Email Subject" "Email Message" "Email recipients"'
+    print 'Pusbullet Example: --pushbullet "Title" "Message"'
     print 'Pushover Example:  --pushover "Message"'
 
 
