48afdbfcada08704601e684f1256bf574b213295
Davide Nastri <d.nastri@gmail.com>
2020-02-15 22:16:41 +0100

    linting: Other fixes

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -15,14 +15,14 @@ def test_cli(runner):
     assert 'Usage: simple-notifications' in result.output.strip()
 
 
-#def test_cli_with_option(runner):
+# def test_cli_with_option(runner):
 #    result = runner.invoke(cli.main, ['--as-cowboy'])
 #    assert not result.exception
 #    assert result.exit_code == 0
 #    assert result.output.strip() == 'Howdy, world.'
-
-
-#def test_cli_with_arg(runner):
+#
+#
+# def test_cli_with_arg(runner):
 #    result = runner.invoke(cli.main, ['Davide'])
 #    assert result.exit_code == 0
 #    assert not result.exception
