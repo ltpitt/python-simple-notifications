44835df2dac4398ea51f33ef2f8740da46d5aa63
Davide Nastri <d.nastri@gmail.com>
2020-02-15 22:48:00 +0100

    feat: Implementing test_cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,30 +7,3 @@ def test_help():
     result = runner.invoke(cli.notification)
     assert result.exit_code == 0
     assert "Usage: " in result.output
-
-
-
-# @pytest.fixture
-# def runner():
-#     return CliRunner()
-#
-#
-# def test_cli(runner):
-#     result = runner.invoke(cli.main)
-#     assert result.exit_code == 0
-#     assert not result.exception
-#     assert 'Usage: simple-notifications' in result.output.strip()
-
-
-# def test_cli_with_option(runner):
-#    result = runner.invoke(cli.main, ['--as-cowboy'])
-#    assert not result.exception
-#    assert result.exit_code == 0
-#    assert result.output.strip() == 'Howdy, world.'
-#
-#
-# def test_cli_with_arg(runner):
-#    result = runner.invoke(cli.main, ['Davide'])
-#    assert result.exit_code == 0
-#    assert not result.exception
-#    assert result.output.strip() == 'Hello, Davide.'
